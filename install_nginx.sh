6d8b932a10be9c4570fabf1f18ca28d7e183df92
root <root@SHUBH.localdomain>
2026-05-05 16:54:48 +0000

    feat: add a new file

---
+++ install_nginx.sh
@@ -0,0 +1,20 @@
+# /bin/bash
+
+
+#############################
+     INSTALL NGINX 
+     BY : HARISH SHARMA 
+     DATE : 05-05-2026
+############################
+
+
+sudo apt install nginx y
+
+
+systemctl status nginx -y
+
+systemctl enable nginx -y
+
+
+echo "great its working "
+
